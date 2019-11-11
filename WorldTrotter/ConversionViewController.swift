216a3d0ef70ea2afafe74d97b7edba9c3715df08
KarlaJu <karla@makingdevs.com>
2019-11-10 19:44:22 -0600

    Añadiendo validacion si el valor esta vacio

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -1,9 +1,15 @@
-//
-//  ConversionViewController.swift
-//  WorldTrotter
-//
-//  Created by MakingDevs on 11/10/19.
-//  Copyright © 2019 MakingDevs. All rights reserved.
-//
+import UIKit
 
-import Foundation
+class ConversionViewController: UIViewController {
+    @IBOutlet var celsiusLabel: UILabel!
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+        //celsiusLabel.text = textField.text
+        if let text = textField.text, !text.isEmpty {
+            celsiusLabel.text = text
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+}
