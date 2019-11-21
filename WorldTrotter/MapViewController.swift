7ca0fff1e7dfc1e05ea4be6f2c67d19cef0713bc
KarlaJu <karla@makingdevs.com>
2019-11-21 17:17:34 -0600

    Agregando el mapa mediante codigo

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -7,7 +7,20 @@
 //
 
 import UIKit
+import MapKit
+
 class MapViewController: UIViewController {
+    
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        //Create a map view
+        mapView = MKMapView()
+        
+        //set is at *the* view of this view controller
+        view = mapView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("MapViewController loaded its view")
